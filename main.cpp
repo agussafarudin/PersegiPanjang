f9a66ea8dc05637607274820c1fde59e2205d985
agussafarudin <shempal.sf@gmail.com>
2019-01-03 17:44:01 +0700

    v-01

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float p,l;
+    cout << "Softeare pembuat Luas Persegi Panjang \n \n";
+    cout << "Masukkan Nilai Panjang =";
+    cin >>p;
+    cout<<" \n";
+    cout << "Masukkan Nilai Lebar   =";
+    cin >>l;
+    cout<<" \n";
+    cout << "Luas Persegi Panjang   =";
+    cout <<(p*l);
+    return 0;
+}
